7629639ea7b4743210d41c89ca1ad725ccee69ed
Bárbara Novaes <101150181+babidln@users.noreply.github.com>
2022-05-25 21:49:49 -0300

    Add files via upload

---
+++ aula12/ConsoleUtils.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ConsoleUtils {
+        public static int lerInteiro(String msg){
+           System.out.println(msg);
+           Scanner input = new Scanner(System.in);
+           while(true){
+           String linhaDigitada = input.next();
+           try {
+                int num = Integer.parseInt(linhaDigitada); 
+                input.close();
+                return num;
+           } catch (NumberFormatException ex){
+               System.out.println("Valor informado não é um número inteiro válido! Tente novamente");
+               //System.out.println(ex.getMessage());
+               //ex.printStackTrace();
+           }
+        }
+    }
+}
